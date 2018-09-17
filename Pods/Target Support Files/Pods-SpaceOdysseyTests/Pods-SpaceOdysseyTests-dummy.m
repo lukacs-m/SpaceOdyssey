04bb7efda53400476cddd70a0abec3dafe49273e
Martin <martin.lukacs@epitech.eu>
2018-09-18 00:21:13 +0200

    Initial commit

---
+++ Pods/Target Support Files/Pods-SpaceOdysseyTests/Pods-SpaceOdysseyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpaceOdysseyTests : NSObject
+@end
+@implementation PodsDummy_Pods_SpaceOdysseyTests
+@end
---
+++ SpaceOdyssey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceOdyssey
+//
+//  Created by Martin Lukacs on 18/09/2018.
+//  Copyright © 2018 martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
